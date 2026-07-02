ecc470b7858197f0acf7e5f2581937f51da0c238
Therion <vinhquangdoan0@gmail.com>
2026-07-02 23:14:06 +0700

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,391 +1,208 @@
+-- BASIC QUERIES
+
+-- 1: Retrieve each table
+SELECT * FROM [USER];
+SELECT * FROM COSTUME;
+SELECT * FROM COSTUME_ITEM;
+SELECT * FROM COSTUME_IMAGE;
+SELECT * FROM ORDER_ITEM;
+SELECT * FROM RENTAL_ORDER;
+SELECT * FROM PAYMENT;
+SELECT * FROM PRICING_POLICY;
 
 
---BASIC QUERIES--
---1: Retrieve each tables
-SELECT * from [USER]
-SELECT * FROM COSTUME
-SELECT * FROM COSTUME_ITEM
-select * from COSTUME_IMAGE
-select * from ORDER_ITEM
-SELECT * FROM RENTAL_ORDER
-SELECT * FROM PAYMENT
-select * from PRICING_POLICY
-
---WHERE queries--
---2: Retrieves with condition
-SELECT *
-FROM [USER]
-WHERE status = 'active'
-
-SELECT *
-FROM [USER]
-WHERE role = 'customer'
-
-SELECT *
-FROM COSTUME_ITEM
-WHERE status = 'available'
-
-SELECT *
-FROM RENTAL_ORDER
-WHERE status = 'active'
-
-SELECT *
-FROM PAYMENT
-WHERE status = 'success'
+-- WHERE QUERIES
+-- 2: Retrieves with condition
+SELECT * FROM [USER] WHERE status = 'active';
+SELECT * FROM [USER] WHERE role = 'customer';
+SELECT * FROM COSTUME_ITEM WHERE status = 'available';
+SELECT * FROM RENTAL_ORDER WHERE status = 'Active'
+SELECT * FROM PAYMENT WHERE status = 'success';
 
 
---ORDER BY queries--
---3 Order by 
-SELECT *
-FROM COSTUME
-ORDER BY dailyRate DESC
---Display costumes from highest rental fee to lowest.
+-- ORDER BY QUERIES
+-- 3: Order by
+-- Display costumes from highest rental fee to lowest.
+SELECT * FROM COSTUME ORDER BY dailyRate DESC;
 
-SELECT *
-FROM [USER]
-ORDER BY fullName ASC
---Sort users alphabetically.
+-- Sort users alphabetically.
+SELECT * FROM [USER] ORDER BY fullName ASC;
 
-SELECT *
-FROM RENTAL_ORDER
-ORDER BY orderDate DESC
---Show newest orders first. 
+-- Show newest orders first.
+SELECT * FROM RENTAL_ORDER ORDER BY orderDate DESC;
 
-SELECT *
-FROM PAYMENT
-ORDER BY amount DESC
---Display highest payments first. 
+-- Display highest payments first.
+SELECT * FROM PAYMENT ORDER BY amount DESC;
 
-SELECT *
-FROM PRICING_POLICY
-ORDER BY effectiveDate DESC
---Show latest policies first. 
+-- Show latest policies first.
+SELECT * FROM PRICING_POLICY ORDER BY effectiveDate DESC;
 
+-- AGGREGATE QUERIES
+-- 4: Count
+-- Count all registered users.
+SELECT COUNT(*) AS TotalUsers FROM [USER];
 
+-- Count rentable costume items.
+SELECT COUNT(*) AS AvailableItems FROM COSTUME_ITEM WHERE status = 'available';
 
---AGGREGATE queries--
---4: Count  
-SELECT COUNT(*) AS TotalUsers
-FROM [USER];
---Count all registered users. 
- 
-SELECT COUNT(*) AS AvailableItems
-FROM COSTUME_ITEM
-WHERE status = 'available'
---Count rentable costume items. 
+-- 5: Calculate Average
+-- Calculate average rental price per day.
+SELECT AVG(dailyRate) AS AverageDailyRate FROM COSTUME;
 
---5: calculate Average
-SELECT AVG(dailyRate) AS AverageDailyRate
-FROM COSTUME
---Calculate average rental price per day. 
+-- 6: Find Maximum
+-- Find the highest payment.
+SELECT MAX(amount) AS HighestPayment FROM PAYMENT;
 
---6: find Maximum 
-SELECT MAX(amount) AS HighestPayment
-FROM PAYMENT;
---Find the highest payment. 
+-- 7: Calculate total
+SELECT SUM(amount) AS TotalRevenue FROM PAYMENT WHERE status = 'success';
 
---7: Calculate total 
-SELECT SUM(amount) AS TotalRevenue
-FROM PAYMENT
-WHERE status = 'success'
---Calculate total revenue from successful payments. 
-
-
---JOIN--
---8: Customer information with rental orders
-SELECT u.fullName,
-       	       ro.orderId,
-      	       ro.orderDate,
-       	       ro.status
+-- JOIN QUERIES
+-- 8: Customer information with rental orders
+SELECT u.fullName, ro.orderId, ro.orderDate, ro.status
 FROM [USER] u
-INNER JOIN RENTAL_ORDER ro
-ON u.userId = ro.customerId
+INNER JOIN RENTAL_ORDER ro ON u.userId = ro.customerId;
 
-
---9: Staff handling orders
-SELECT s.fullName,
-     	     ro.orderId,
-      	     ro.status
+-- 9: Staff handling orders
+SELECT s.fullName, ro.orderId, ro.status
 FROM [USER] s
-INNER JOIN RENTAL_ORDER ro
-ON s.userId = ro.staffId
+INNER JOIN RENTAL_ORDER ro ON s.userId = ro.staffId;
 
-
---10: Costume items with costume names
-SELECT ci.itemId,
-      	      c.name,
-      ci.size,
-      ci.color,
-      ci.status
+-- 10: Costume items with costume names
+SELECT ci.itemId, c.name, ci.size, ci.color, ci.status
 FROM COSTUME_ITEM ci
-INNER JOIN COSTUME c
-ON ci.costumeId = c.costumeId;
+INNER JOIN COSTUME c ON ci.costumeId = c.costumeId;
 
-
---11: Rental orders with pricing policies
-SELECT ro.orderId,
-      	      pp.policyName,
-      	      pp.lateFeeRate
+-- 11: Rental orders with pricing policies
+SELECT ro.orderId, pp.policyName, pp.lateFeeRate
 FROM RENTAL_ORDER ro
-INNER JOIN PRICING_POLICY pp
-ON ro.policyId = pp.policyId
+INNER JOIN PRICING_POLICY pp ON ro.policyId = pp.policyId;
 
---12: Orders and payments
-SELECT ro.orderId,
-     	     p.paymentId,
-     	     p.amount,
-     	     p.status
+-- 12: Orders and payments
+SELECT ro.orderId, p.paymentId, p.amount, p.status
 FROM RENTAL_ORDER ro
-INNER JOIN PAYMENT p
-ON ro.orderId = p.orderId
+INNER JOIN PAYMENT p ON ro.orderId = p.orderId;
 
---GROUP BY AND HAVING--
---13: Number of orders per customer
-SELECT customerId,
-      COUNT(*) AS TotalOrders
+
+
+-- GROUP BY AND HAVING QUERIES 
+-- 13: Number of orders per customer
+SELECT customerId, COUNT(*) AS TotalOrders
 FROM RENTAL_ORDER
 GROUP BY customerId;
 
---14: Customers with more than 2 orders
-SELECT customerId,
-      COUNT(*) AS TotalOrders
+-- 14: Customers with at least 1 order
+SELECT customerId, COUNT(*) AS TotalOrders
 FROM RENTAL_ORDER
 GROUP BY customerId
-HAVING COUNT(*) > 2;
+HAVING COUNT(*) >= 1;
 
---15: Total revenue by payment method
-SELECT paymentMethod,
-      SUM(amount) AS Revenue
+-- 15: Total revenue by payment method
+SELECT paymentMethod, SUM(amount) AS Revenue
 FROM PAYMENT
 GROUP BY paymentMethod;
 
+-- 16: Average costume rental price by theme 
+SELECT theme, AVG(dailyRate) AS AvgRate
+FROM COSTUME
+GROUP BY theme;
 
---15: Average costume rental price by theme
-SELECT theme,
-      AVG(dailyRate) AS AvgRate
+-- 17: Number of costumes in each theme
+SELECT theme, COUNT(*) AS TotalCostumes
 FROM COSTUME
 GROUP BY theme;
 
 
---16: Number of costumes in each theme
-SELECT theme,
-      COUNT(*) AS TotalCostumes
-FROM COSTUME
-GROUP BY theme;
+-- QUERIES CONTAIN SUBQUERIES
 
 
---QUERIES CONTAIN SUBQUERIES
---17: Costumes with rental price above average
-SELECT *
-FROM COSTUME
-WHERE dailyRate >
-(
-   SELECT AVG(dailyRate)
-   FROM COSTUME
-);
+-- 18: Costumes with rental price above average
+SELECT * FROM COSTUME 
+WHERE dailyRate > (SELECT AVG(dailyRate) FROM COSTUME);
 
---18: Customers who have placed orders
-SELECT *
-FROM [USER]
-WHERE userId IN
-(
-   SELECT customerId
-   FROM RENTAL_ORDER
-);
+-- 19: Customers who have placed orders
+SELECT * FROM [USER] 
+WHERE userId IN (SELECT customerId FROM RENTAL_ORDER);
 
+-- 20: Customers who never rented
+SELECT * FROM [USER] 
+WHERE role = 'customer' AND userId NOT IN (SELECT customerId FROM RENTAL_ORDER);
 
---19: Customers who never rented
-SELECT *
-FROM [USER]
-WHERE role = 'customer' and userId NOT IN 
-(
-   SELECT customerId
-   FROM RENTAL_ORDER
-);
+-- 21: Orders with highest total fee
+SELECT * FROM RENTAL_ORDER 
+WHERE totalFee = (SELECT MAX(totalFee) FROM RENTAL_ORDER);
 
---20: Orders with highest total fee
-SELECT *
-FROM RENTAL_ORDER
-WHERE totalFee =
-(
-   SELECT MAX(totalFee)
-   FROM RENTAL_ORDER
-);
+-- 22: Payments larger than average payment
+SELECT * FROM PAYMENT 
+WHERE amount > (SELECT AVG(amount) FROM PAYMENT);
 
---21: Payments larger than average payment
-SELECT *
-FROM PAYMENT
-WHERE amount >
-(
-   SELECT AVG(amount)
-   FROM PAYMENT
-);
+-- 23: Most expensive costume
+SELECT * FROM COSTUME 
+WHERE dailyRate = (SELECT MAX(dailyRate) FROM COSTUME);
 
---22: Most expensive costume
-SELECT *
-FROM COSTUME
-WHERE dailyRate =
-(
-   SELECT MAX(dailyRate)
-   FROM COSTUME
-);
+-- 24: Orders using newest policy
+SELECT * FROM RENTAL_ORDER 
+WHERE policyId = (SELECT TOP 1 policyId FROM PRICING_POLICY ORDER BY effectiveDate DESC);
 
---23: Orders using newest policy
-SELECT *
-FROM RENTAL_ORDER
-WHERE policyId =
-(
-   SELECT TOP 1 policyId
-   FROM PRICING_POLICY
-   ORDER BY effectiveDate DESC
-);
+-- 25: Available items belonging to expensive costumes 
+SELECT * FROM COSTUME_ITEM
+WHERE costumeId IN (SELECT costumeId FROM COSTUME WHERE dailyRate > 150000);
 
---24: Available items belonging to expensive costumes
-SELECT *
-FROM COSTUME_ITEM
-WHERE costumeId IN
-(
-   SELECT costumeId
-   FROM COSTUME
-   WHERE dailyRate > 500000
-);
+-- 26: Payments for active orders (Sửa 'active' thành 'Active' khớp data mới)
+SELECT * FROM PAYMENT 
+WHERE orderId IN (SELECT orderId FROM RENTAL_ORDER WHERE status = 'Active');
 
---25: Payments for active orders
-SELECT *
-FROM PAYMENT
-WHERE orderId IN
-(
-   SELECT orderId
-   FROM RENTAL_ORDER
-   WHERE status = 'active'
-);
-
---26: Orders with total fee above average
-SELECT *
-FROM RENTAL_ORDER
-WHERE totalFee >
-(
-   SELECT AVG(totalFee)
-   FROM RENTAL_ORDER
-);
+-- 27: Orders with total fee above average
+SELECT * FROM RENTAL_ORDER 
+WHERE totalFee > (SELECT AVG(totalFee) FROM RENTAL_ORDER);
 
 
-
-
-
---ADVANCED QUERIES
---27: Customers with orders having total fee above average
-SELECT 
-    u.userId,
-    u.fullName AS username,
-    ro.totalFee
+-- ADVANCED QUERIES & SUBQUERIES
+-- 28: Customers with orders having total fee above average
+SELECT u.userId, u.fullName AS username, ro.totalFee
 FROM [USER] u
 INNER JOIN RENTAL_ORDER ro ON u.userId = ro.customerId
-WHERE ro.totalFee > 
-(
-    SELECT AVG(totalFee)
-    FROM RENTAL_ORDER
-);
+WHERE ro.totalFee > (SELECT AVG(totalFee) FROM RENTAL_ORDER);
 
---28: Costumes rented in the most expensive order
-SELECT *
-FROM COSTUME
-WHERE costumeId IN
-(
-   SELECT costumeId
-   FROM COSTUME_ITEM
-   WHERE itemId IN
-   (
-       SELECT itemId
-       FROM ORDER_ITEM
-       WHERE orderId =
-       (
-           SELECT TOP 1 orderId
-           FROM RENTAL_ORDER
-           ORDER BY totalFee DESC
+-- 29: Costumes rented in the most expensive order
+SELECT * FROM COSTUME
+WHERE costumeId IN (
+   SELECT costumeId FROM COSTUME_ITEM WHERE itemId IN (
+       SELECT itemId FROM ORDER_ITEM WHERE orderId = (
+           SELECT TOP 1 orderId FROM RENTAL_ORDER ORDER BY totalFee DESC
        )
    )
 );
---29: Payments related to orders with highest fee
-SELECT *
-FROM PAYMENT
-WHERE orderId IN
-(
-   SELECT orderId
-   FROM RENTAL_ORDER
-   WHERE totalFee =
-   (
-       SELECT MAX(totalFee)
-       FROM RENTAL_ORDER
-   )
-);
 
---30: Policies used by above-average orders
-SELECT *
-FROM PRICING_POLICY
-WHERE policyId IN
-(
-   SELECT policyId
-   FROM RENTAL_ORDER
-   WHERE totalFee >
-   (
-       SELECT AVG(totalFee)
-       FROM RENTAL_ORDER
-   )
-)
+-- 30: Payments related to orders with highest fee
+SELECT * FROM PAYMENT 
+WHERE orderId IN (SELECT orderId FROM RENTAL_ORDER WHERE totalFee = (SELECT MAX(totalFee) FROM RENTAL_ORDER));
+
+-- 31: Policies used by above-average orders
+SELECT * FROM PRICING_POLICY 
+WHERE policyId IN (SELECT policyId FROM RENTAL_ORDER WHERE totalFee > (SELECT AVG(totalFee) FROM RENTAL_ORDER));
 
 
---IN, ANY, ALL Queries
---31: Customers with active orders
-SELECT *
-FROM [USER]
-WHERE userId IN
-(
-   SELECT customerId
-   FROM RENTAL_ORDER
-   WHERE status = 'active'
-);
+-- IN, ANY, ALL QUERIES
+-- 32: Customers with active orders (Sửa 'active' thành 'Active')
+SELECT * FROM [USER] 
+WHERE userId IN (SELECT customerId FROM RENTAL_ORDER WHERE status = 'Active');
 
---32: Payments greater than ANY order fee
-SELECT *
-FROM PAYMENT
-WHERE amount > ANY
-(
-   SELECT totalFee
-   FROM RENTAL_ORDER
-);
+-- 33: Payments greater than ANY order fee
+SELECT * FROM PAYMENT 
+WHERE amount > ANY (SELECT totalFee FROM RENTAL_ORDER);
+
+-- 34: Payments greater than ALL order fees
+SELECT * FROM PAYMENT 
+WHERE amount > ALL (SELECT totalFee FROM RENTAL_ORDER);
 
 
---33: Payments greater than ALL order fees
-SELECT *
-FROM PAYMENT
-WHERE amount > ALL
-(
-   SELECT totalFee
-   FROM RENTAL_ORDER
-);
-
-
---SET OPERATION QUERIES
---34: All customers and staff names
-SELECT fullName
-FROM [USER]
-WHERE role = 'customer'
-
+-- SET OPERATION QUERIES
+-- 35: All customers and staff names
+SELECT fullName FROM [USER] WHERE role = 'customer'
 UNION
+SELECT fullName FROM [USER] WHERE role = 'staff';
 
-SELECT fullName
-FROM [USER]
-WHERE role = 'staff';
-
---35: Users who are customer or admin
-SELECT userId
-FROM [USER]
-WHERE role = 'customer'
-
+-- 36: Users who are customer or admin
+SELECT userId FROM [USER] WHERE role = 'customer'
 UNION
-
-SELECT userId
-FROM [USER]
-WHERE role = 'admin';
-
+SELECT userId FROM [USER] WHERE role = 'admin';
