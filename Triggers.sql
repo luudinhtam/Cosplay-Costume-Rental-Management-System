a924712484ad646ec2913d602d86f11ec0e7af34
Tam Luu Dinh <116478073+luudinhtam@users.noreply.github.com>
2026-07-05 20:12:25 +0700

    Update and rename Trigger.sql to Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -1,4 +1,5 @@
 --1 This trigger prevents deleting costume items that are referenced in rental history
+GO
 CREATE TRIGGER trg_PreventDeleteCostumeItem
 ON COSTUME_ITEM
 INSTEAD OF DELETE
@@ -15,6 +16,7 @@ BEGIN
     )
     BEGIN
         RAISERROR('Cannot delete a costume item because it has been used in rental orders.',16,1);
+        rollback transaction
         RETURN;
     END;
 
@@ -28,7 +30,17 @@ BEGIN
 END;
 GO
 
+-- Test 
+DELETE FROM COSTUME_ITEM WHERE itemId = 2; 
+-- Expected output: System sẽ không cho delete vì item có id = 2 đang được cho thuê
+
+-- Check
+SELECT * FROM COSTUME_ITEM;
+
+
+
 --2 This trigger automatically updates the costume item status after it is rented
+GO
 CREATE TRIGGER trg_UpdateItemStatusAfterRent
 ON ORDER_ITEM
 AFTER INSERT
@@ -46,7 +58,22 @@ BEGIN
 END;
 GO
 
+-- Test and Check
+BEGIN TRAN; -- Item đang 'available' -> insert vào ORDER_ITEM -> phải đổi thành 'rented'
+
+SELECT * FROM COSTUME_ITEM WHERE itemId = 11; -- trước: available
+
+INSERT INTO ORDER_ITEM (orderId, itemId, statusBefore, statusAfter, itemDailyRate)
+VALUES (9, 11, '100% New', NULL, 130000.00); -- dùng lại orderId 9 (Pending) cho tiện
+
+SELECT * FROM COSTUME_ITEM WHERE itemId = 11; -- sau: mong muốn 'rented'
+
+ROLLBACK;
+
+
+
 --3 This trigger restores costume availability after the rental process is completed
+GO
 CREATE TRIGGER trg_ReturnCostumeItem
 ON RENTAL_ORDER
 AFTER UPDATE
@@ -64,12 +91,29 @@ BEGIN
     INNER JOIN deleted d
         ON d.orderId = i.orderId
     WHERE
-        d.status <> 'Completed'
-        AND i.status = 'Completed';
+        d.status <> 'Returned'
+        AND i.status = 'Returned';
 END;
 GO
 
---4 This trigger blocks invalid payment records with negative amounts
+-- Test and Check
+BEGIN TRAN;
+
+select * from RENTAL_ORDER where orderId = 6 -- Đơn số 6 đang active
+
+SELECT * FROM ORDER_ITEM where orderId = 6 -- Đơn số 6 có item id = 8 đang được cho thuê rented
+
+SELECT * FROM COSTUME_ITEM where itemId = 8 -- item id = 8 đang được cho thuê rented
+
+UPDATE RENTAL_ORDER SET status = 'Returned' WHERE orderId = 6; -- Khi đơn số 6 được thuê xong returned
+
+SELECT * FROM COSTUME_ITEM WHERE itemId = 8; -- item id = 8 của đơn số 6 sẽ có status là available
+
+ROLLBACK
+
+
+--4 
+GO
 CREATE TRIGGER trg_CheckMaxActiveRentals
 ON RENTAL_ORDER
 AFTER INSERT
@@ -95,4 +139,8 @@ BEGIN
     END
 END;
 GO
+
+-- Test and Check
+
+
 
